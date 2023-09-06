2bc01981e0865d3c83209d90c4df6601e4e49305
JordanR94 <44776172+JordanR94@users.noreply.github.com>
2023-09-06 09:56:07 +0100

    video 18 sluggable

--- src/Controller/MixController.php
+++ src/Controller/MixController.php
@@ -35,7 +35,7 @@ class MixController extends AbstractController
         ));
     }
 
-    #[Route('/mix/{id}', name: 'app_mix_show')]
+    #[Route('/mix/{slug}', name: 'app_mix_show')]
     public function show(VinylMix $mix)
     {
         return $this->render('mix/show.html.twig', [
@@ -59,7 +59,7 @@ class MixController extends AbstractController
         $this->addFlash('success', 'Vote Counted!');
 
         return $this->redirectToRoute('app_mix_show', [
-           'id' => $mix->getId(),
+           'slug' => $mix->getSlug(),
         ]);
     }
 
