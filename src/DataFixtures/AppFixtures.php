8614a3278da0513dc3bfd7b7adf0521e0b3b5510
JordanR94 <44776172+JordanR94@users.noreply.github.com>
2023-09-06 10:45:56 +0100

    video 20 foundry

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\VinylMix;
+use App\Factory\VinylMixFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -10,16 +11,6 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $mix = new VinylMix();
-        $mix->setTitle('Testing out the title');
-        $mix->setDescription('I have no idea what to write');
-        $genres = ['pop', 'rock'];
-        $mix->setGenre($genres[array_rand($genres)]);
-        $mix->setTrackCount(rand(5, 20));
-        $mix->setVotes(rand(-50, 50));
-
-        $manager->persist($mix);
-
-        $manager->flush();
+        VinylMixFactory::createMany(25);
     }
 }
